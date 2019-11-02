7d7844cba3c80fc625677fe07b01ced21db2ef09
cuishuhao123 <47618405+cuishuhao123@users.noreply.github.com>
2019-11-02 16:18:41 +0800

    new

---
+++ F1.Designer.vb
@@ -0,0 +1,347 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class F1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(F1))
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.登录ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.急速ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.退出ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AV之家ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.开始ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.打开本地ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.推荐ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.登录ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.关闭ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.激活ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.关于ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AV之家ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.登录ToolStripMenuItem, Me.急速ToolStripMenuItem, Me.退出ToolStripMenuItem, Me.AV之家ToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(117, 92)
+        '
+        '登录ToolStripMenuItem
+        '
+        Me.登录ToolStripMenuItem.Name = "登录ToolStripMenuItem"
+        Me.登录ToolStripMenuItem.Size = New System.Drawing.Size(116, 22)
+        Me.登录ToolStripMenuItem.Text = "登录"
+        '
+        '急速ToolStripMenuItem
+        '
+        Me.急速ToolStripMenuItem.Name = "急速ToolStripMenuItem"
+        Me.急速ToolStripMenuItem.Size = New System.Drawing.Size(116, 22)
+        Me.急速ToolStripMenuItem.Text = "急速"
+        '
+        '退出ToolStripMenuItem
+        '
+        Me.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem"
+        Me.退出ToolStripMenuItem.Size = New System.Drawing.Size(116, 22)
+        Me.退出ToolStripMenuItem.Text = "退出"
+        '
+        'AV之家ToolStripMenuItem
+        '
+        Me.AV之家ToolStripMenuItem.Name = "AV之家ToolStripMenuItem"
+        Me.AV之家ToolStripMenuItem.Size = New System.Drawing.Size(116, 22)
+        Me.AV之家ToolStripMenuItem.Text = "AV之家"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.开始ToolStripMenuItem, Me.激活ToolStripMenuItem, Me.关于ToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(773, 25)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        '开始ToolStripMenuItem
+        '
+        Me.开始ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.打开本地ToolStripMenuItem, Me.推荐ToolStripMenuItem, Me.登录ToolStripMenuItem1, Me.关闭ToolStripMenuItem})
+        Me.开始ToolStripMenuItem.Name = "开始ToolStripMenuItem"
+        Me.开始ToolStripMenuItem.Size = New System.Drawing.Size(44, 21)
+        Me.开始ToolStripMenuItem.Text = "开始"
+        '
+        '打开本地ToolStripMenuItem
+        '
+        Me.打开本地ToolStripMenuItem.Name = "打开本地ToolStripMenuItem"
+        Me.打开本地ToolStripMenuItem.Size = New System.Drawing.Size(124, 22)
+        Me.打开本地ToolStripMenuItem.Text = "打开本地"
+        '
+        '推荐ToolStripMenuItem
+        '
+        Me.推荐ToolStripMenuItem.Name = "推荐ToolStripMenuItem"
+        Me.推荐ToolStripMenuItem.Size = New System.Drawing.Size(124, 22)
+        Me.推荐ToolStripMenuItem.Text = "推荐"
+        '
+        '登录ToolStripMenuItem1
+        '
+        Me.登录ToolStripMenuItem1.Name = "登录ToolStripMenuItem1"
+        Me.登录ToolStripMenuItem1.Size = New System.Drawing.Size(124, 22)
+        Me.登录ToolStripMenuItem1.Text = "登录"
+        '
+        '关闭ToolStripMenuItem
+        '
+        Me.关闭ToolStripMenuItem.Name = "关闭ToolStripMenuItem"
+        Me.关闭ToolStripMenuItem.Size = New System.Drawing.Size(124, 22)
+        Me.关闭ToolStripMenuItem.Text = "关闭"
+        '
+        '激活ToolStripMenuItem
+        '
+        Me.激活ToolStripMenuItem.Name = "激活ToolStripMenuItem"
+        Me.激活ToolStripMenuItem.Size = New System.Drawing.Size(44, 21)
+        Me.激活ToolStripMenuItem.Text = "激活"
+        '
+        '关于ToolStripMenuItem
+        '
+        Me.关于ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AV之家ToolStripMenuItem1})
+        Me.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem"
+        Me.关于ToolStripMenuItem.Size = New System.Drawing.Size(44, 21)
+        Me.关于ToolStripMenuItem.Text = "关于"
+        '
+        'AV之家ToolStripMenuItem1
+        '
+        Me.AV之家ToolStripMenuItem1.Name = "AV之家ToolStripMenuItem1"
+        Me.AV之家ToolStripMenuItem1.Size = New System.Drawing.Size(116, 22)
+        Me.AV之家ToolStripMenuItem1.Text = "AV之家"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox2)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(322, 44)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(439, 332)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Visible = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(59, 35)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 12)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "用户名"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(59, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 12)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "密码"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(261, 35)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(126, 21)
+        Me.TextBox2.TabIndex = 6
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(261, 75)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(126, 21)
+        Me.TextBox1.TabIndex = 5
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"视频", "文字", "声音", "图片", "漫画"})
+        Me.ComboBox2.Location = New System.Drawing.Point(266, 275)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 20)
+        Me.ComboBox2.TabIndex = 4
+        Me.ComboBox2.Text = "类别"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"欧美", "日韩", "澳洲", "大陆", "香港", "东南亚", "非洲（hot）"})
+        Me.ComboBox1.Location = New System.Drawing.Point(61, 275)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 20)
+        Me.ComboBox1.TabIndex = 3
+        Me.ComboBox1.Text = "播放源"
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("宋体", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button3.Location = New System.Drawing.Point(61, 168)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(326, 80)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "游客"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(261, 119)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(126, 32)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "注册"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(61, 119)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(126, 32)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "登录"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 44)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(304, 190)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 415)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(749, 23)
+        Me.ProgressBar1.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(10, 390)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 12)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "请激活"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Filter = "AV文件|*.avlover|all|*.*"
+        Me.OpenFileDialog1.Title = "打开文件"
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(12, 240)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(304, 136)
+        Me.WebBrowser1.TabIndex = 6
+        Me.WebBrowser1.Visible = False
+        '
+        'F1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(773, 450)
+        Me.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "F1"
+        Me.ShowInTaskbar = False
+        Me.Text = "免费AV"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents 登录ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 急速ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 退出ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AV之家ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents 开始ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 打开本地ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 推荐ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 登录ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents 关闭ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 激活ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 关于ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AV之家ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+End Class
---
+++ F1.vb
@@ -0,0 +1,82 @@
+﻿Public Class F1
+    Private Sub 激活ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 激活ToolStripMenuItem.Click
+        Dim jihuo As String = InputBox("激活码：", "激活")
+        Me.ProgressBar1.Value = 30
+        If jihuo = "123" Then
+            Me.GroupBox1.Visible = True
+            Me.ProgressBar1.Value = 60
+            Me.Label1.Text = "就绪"
+            Me.ProgressBar1.Value = 100
+            Threading.Thread.Sleep(600)
+            Me.ProgressBar1.Value = 0
+        End If
+
+    End Sub
+
+    Private Sub F1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("激活码为123", , "我爱破解赞助")
+        MsgBox("请输入激活码")
+    End Sub
+
+    Private Sub 打开本地ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 打开本地ToolStripMenuItem.Click
+        If (Me.OpenFileDialog1.ShowDialog() = DialogResult.OK) And (OpenFileDialog1.FileName.Length > 0) Then
+            MsgBox（"无法打开"）
+        End If
+    End Sub
+
+    Private Sub 推荐ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 推荐ToolStripMenuItem.Click
+        Me.WebBrowser1.Visible = True
+    End Sub
+
+    Private Sub 登录ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles 登录ToolStripMenuItem1.Click
+        Me.WebBrowser1.Visible = True
+    End Sub
+
+    Private Sub 关闭ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 关闭ToolStripMenuItem.Click
+        MsgBox("请观看3分钟")
+    End Sub
+
+    Private Sub AV之家ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles AV之家ToolStripMenuItem1.Click
+        Me.WebBrowser1.Visible = True
+    End Sub
+
+    Private Sub 登录ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 登录ToolStripMenuItem.Click
+        Me.WebBrowser1.Visible = True
+    End Sub
+
+    Private Sub 急速ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 急速ToolStripMenuItem.Click
+        Me.WebBrowser1.Visible = True
+    End Sub
+
+    Private Sub AV之家ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AV之家ToolStripMenuItem.Click
+        Me.WebBrowser1.Visible = True
+    End Sub
+
+    Private Sub 退出ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 退出ToolStripMenuItem.Click
+        MsgBox("请观看3分钟")
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Label1.Text = ("无效用户")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Label1.Text = "已提交申请，3小时后通过，请游客登录"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Label1.Text = "请等待"
+        Me.ProgressBar1.Value = 10
+        Threading.Thread.Sleep(20)
+        Me.ProgressBar1.Value = 40
+        Threading.Thread.Sleep(20)
+        Me.ProgressBar1.Value = 60
+        Threading.Thread.Sleep(20)
+        Me.ProgressBar1.Value = 80
+        Me.Label1.Text = "已打开"
+        Threading.Thread.Sleep(20)
+        Me.ProgressBar1.Value = 100
+        Threading.Thread.Sleep(20)
+    End Sub
+End Class

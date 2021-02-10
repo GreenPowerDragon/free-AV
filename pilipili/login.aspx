d6ece6799b4b86af028cb82ad394ededd78ec8e9
cuishuhao <cl6702@outlook.com>
2021-02-11 00:51:03 +0800

    begin to debug

--- pilipili/login.aspx
+++ pilipili/login.aspx
@@ -1,3 +1,52 @@
 ﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="login.aspx.vb" Inherits="pilipili.login" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+        &nbsp;</p>
+    <asp:Panel ID="Panel2" runat="server">
+        <p>
+            用户名：<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            密码&nbsp;&nbsp; ：<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:CheckBox ID="CheckBox1" runat="server" AutoPostBack="True" Text="没有账号，注册一个" />
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="提交" />
+        </p>
+    </asp:Panel>
+    <asp:Panel ID="Panel1" runat="server" Visible="False">
+        中文名：<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <br />
+        出生地 ：<asp:DropDownList ID="DropDownList1" runat="server">
+            <asp:ListItem Value="美洲"></asp:ListItem>
+            <asp:ListItem Value="非洲"></asp:ListItem>
+            <asp:ListItem Value="欧洲"></asp:ListItem>
+            <asp:ListItem Value="大洋洲"></asp:ListItem>
+            <asp:ListItem Value="南极洲"></asp:ListItem>
+            <asp:ListItem>亚洲</asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        年龄&nbsp;&nbsp;&nbsp; ：<asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        <br />
+        邮箱&nbsp;&nbsp;&nbsp; ：<asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+        <asp:Button ID="Button2" runat="server" Text="发送验证码" Width="85px" />
+        <br />
+        验证码 ：<asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+        <br />
+        填写完成请点击提交<br /> 确保你是男性</asp:Panel>
+    <asp:Panel ID="Panel3" runat="server" Visible="False">
+        请如实选择<br /> 
+        <asp:Image ID="Image1" runat="server" Height="309px" Width="298px" />
+        <br />
+        <asp:Button ID="Button4" runat="server" Text="喜欢" />
+        <asp:Button ID="Button5" runat="server" Text="不喜欢" />
+        <br /> 
+        <br />
+        为了确保你是男人，请上传那里的照片<br /> 
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <br />
+        <asp:Button ID="Button3" runat="server" Text="开始" />
+    </asp:Panel>
 </asp:Content>
--- pilipili/login.aspx.vb
+++ pilipili/login.aspx.vb
@@ -1,8 +1,75 @@
 ﻿Public Class login
     Inherits System.Web.UI.Page
-
+    Dim num As Integer
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
 
+
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.TextBox1.Text = "" And Me.TextBox2.Text = "" And Me.TextBox3.Text = "" And Me.TextBox5.Text = "" Then
+            Response.Write("<Script Language=JavaScript>alert('请填写完整');</Script>")
+        Else
+            If Int(Me.TextBox4.Text) >= 18 Then
+                Me.Panel1.Visible = False
+                Me.Panel2.Visible = False
+                Me.Panel3.Visible = True
+            Else
+                Response.Write("<Script Language=JavaScript>alert('你还太小');</Script>")
+            End If
+        End If
+    End Sub
+
+    Protected Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If Me.CheckBox1.Checked = True Then
+            Me.Panel1.Visible = True
+        Else
+            Me.Panel1.Visible = False
+        End If
+    End Sub
+
+    Protected Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Button1.Enabled = False
+        Response.Write("<Script Language=JavaScript>alert('请等待30秒');</Script>")
+        Threading.Thread.Sleep(3000)
+        Me.Button1.Enabled = True
+        Response.Write("<Script Language=JavaScript>alert('邮箱服务器错误，你无需输入验证码');</Script>")
+    End Sub
+
+    Protected Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If Me.FileUpload1.HasFile = False Then
+            Response.Write("<Script Language=JavaScript>alert('请上传照片，我们不会告诉任何人！');</Script>")
+        Else
+            If num >= 15 Then
+                Response.Cookies（"login"）.Value = "123"
+                Response.Write("<Script Language=JavaScript>window.location.href='./enjoy.aspx';</Script>")
+            Else
+                Response.Write("<Script Language=JavaScript>alert('请必须选完！');</Script>")
+            End If
+        End If
+    End Sub
+
+    Protected Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        num = num + 1
+        Response.Write("<Script Language=JavaScript>alert('已选择喜欢');</Script>")
+        If num = 15 Then
+            Response.Write("<Script Language=JavaScript>alert('选完了');</Script>")
+            Me.Button4.Enabled = False
+            Me.Button5.Enabled = False
+        End If
+        Me.Image1.ImageUrl = "./imge/" + num + ".jpg"
+
+    End Sub
+
+    Protected Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        num = num + 1
+        Response.Write("<Script Language=JavaScript>alert('已选择不喜欢');</Script>")
+        If num = 15 Then
+            Response.Write("<Script Language=JavaScript>alert('选完了');</Script>")
+            Me.Button4.Enabled = False
+            Me.Button5.Enabled = False
+        End If
+        Me.Image1.ImageUrl = "./imge/" + num + ".jpg"
+    End Sub
 End Class
